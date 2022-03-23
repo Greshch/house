949ed4cc635bd12121e0366d5dafe5f2bceaeded
Greshch <greshchook@gmail.com>
2022-03-23 19:07:02 +0200

    define consts

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+char const LEFT_UP    = '*';
+char const RIGHT_UP   = '*';
+char const LEFT_DOWN  = '*';
+char const RIGHT_DOWN = '*';
+char const HORIZONT   = '-';
+char const VERTICAL   = '|';
+
+int main()
+{
+    return 0;
+}
